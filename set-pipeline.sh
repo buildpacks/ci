45933e93bb1a2d10c055a114fef9500a9affa108
Matthew McNew <mmcnew@pivotal.io>
2019-02-05 17:20:43 -0500

    Create Concourse Pipeline that tests lifecycle images with pack acceptance tests
    Signed-off-by: Ashwin Venkatesh <avenkatesh@pivotal.io>

---
+++ set-pipeline.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+lpass_note() {
+  lpass show --notes "Shared-Build Service/$1"
+}
+
+dir="$(cd "$(dirname "$0")" && pwd)"
+
+concourse_service_account_key=$(lpass_note 'cncf-concourse-service-account-json-key')
+
+fly -t private set-pipeline \
+  -p buildpack \
+  --var concourse-service-account-key-json="$concourse_service_account_key" \
+  -c <(cat $dir/resources.yml && cat $dir/jobs.yml)

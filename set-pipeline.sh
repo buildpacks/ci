46b934e14eee8404160dda5ab401af9dc48f260a
Danny Joyce <djoyce@pivotal.io>
2019-02-25 15:00:51 -0500

    Add task to package pack release binaries
    Signed-off-by: Andrew Meyer <ameyer@pivotal.io>

--- set-pipeline.sh
+++ set-pipeline.sh
@@ -8,7 +8,7 @@ dir="$(cd "$(dirname "$0")" && pwd)"
 
 concourse_service_account_key=$(lpass_note 'cncf-concourse-service-account-json-key')
 
-fly -t private set-pipeline \
+fly -t buildpacksio set-pipeline \
   -p buildpack \
   --var concourse-service-account-key-json="$concourse_service_account_key" \
   -c <(cat $dir/resources.yml && cat $dir/jobs.yml)

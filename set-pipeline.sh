161a96ecfeab764d5446abd0585c1a710c1551f7
Matthew McNew <mmcnew@pivotal.io>
2019-02-27 15:58:59 -0600

    Merge acceptance jobs into one
    - Trigger on both new pack build and new lifecycle build
    
    Signed-off-by: Matthew McNew <mmcnew@pivotal.io>
    Signed-off-by: Andrew Meyer <ameyer@pivotal.io>

--- set-pipeline.sh
+++ set-pipeline.sh
@@ -8,6 +8,8 @@ dir="$(cd "$(dirname "$0")" && pwd)"
 
 concourse_service_account_key=$(lpass_note 'cncf-concourse-service-account-json-key')
 
+cat <(cat $dir/resources.yml && cat $dir/jobs.yml) | pbcopy
+
 fly -t buildpacksio set-pipeline \
   -p buildpack \
   --var concourse-service-account-key-json="$concourse_service_account_key" \

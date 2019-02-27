f624d1f8c70e2584076c647e843e336432cab973
Matthew McNew <mmcnew@pivotal.io>
2019-02-27 17:00:13 -0600

    Combine pack and lifecycle to be released in lockstep
    - Adds token for pack github release
    
    Signed-off-by: Matthew McNew <mmcnew@pivotal.io>
    Signed-off-by: Andrew Meyer <ameyer@pivotal.io>

--- set-pipeline.sh
+++ set-pipeline.sh
@@ -7,10 +7,12 @@ lpass_note() {
 dir="$(cd "$(dirname "$0")" && pwd)"
 
 concourse_service_account_key=$(lpass_note 'cncf-concourse-service-account-json-key')
+pack_github_release_token=$(lpass_note 'buildpack-pack-cli-github-release-token')
 
 cat <(cat $dir/resources.yml && cat $dir/jobs.yml) | pbcopy
 
 fly -t buildpacksio set-pipeline \
   -p buildpack \
   --var concourse-service-account-key-json="$concourse_service_account_key" \
+  --var pack-github-release-token="$pack_github_release_token" \
   -c <(cat $dir/resources.yml && cat $dir/jobs.yml)

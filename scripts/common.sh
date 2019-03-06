2120cb98a1a132f630933cc7e10abdf74bd775f5
Emily Casey <ecasey@pivotal.io>
2019-03-06 15:37:16 -0500

    try aufs driver
    Signed-off-by: Emily Casey <ecasey@pivotal.io>

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 source /opt/resource/common.sh
-start_docker 3 3
+start_docker 3 3 "" "--storage-driver=aufs"
 
 docker_run() {
     docker run -v /var/run/docker.sock:/var/run/docker.sock --network host -v $(pwd):/workspace -w=/workspace "$@"

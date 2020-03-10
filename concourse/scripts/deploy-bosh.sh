0c42dc1bfc8cb970b5c15cb07bc1afa1d7a690ca
Natalie Arellano <narellano@vmware.com>
2020-03-10 12:26:00 -0400

    Updated concourse deployment metadata
    Signed-off-by: Simon Jones <simonjones@vmware.com>

--- concourse/scripts/deploy-bosh.sh
+++ concourse/scripts/deploy-bosh.sh
@@ -4,5 +4,8 @@ set +x
 set -e
 
 export BBL_GCP_SERVICE_ACCOUNT_KEY=$(lpass show --note 'Shared-Cloud Native Buildpacks/cncf-bbl-concourse-gcp-service-account')
+
+echo "We're not too sure the latest bbl works. For guaranteed satisfaction, you might check out version 7.4.0"
+
 bbl plan --lb-type concourse --iaas gcp --gcp-region us-east1
 bbl up --debug
--- concourse/scripts/fetch-bosh-state.sh
+++ concourse/scripts/fetch-bosh-state.sh
@@ -6,14 +6,13 @@ lpass show --note 'Shared-Cloud Native Buildpacks/cncf-bbl-state' > "$dir/bbl-st
 
 mkdir -p "$dir/vars"
 
-lpass show 'Shared-Cloud Native Buildpacks/bbl/vars' -q --attach att-2702221144863353409-9583 > "$dir"/vars/bbl.tfvars
-lpass show 'Shared-Cloud Native Buildpacks/bbl/vars' -q --attach att-2702221144863353409-11137 > "$dir"/vars/bosh-state.json
-lpass show 'Shared-Cloud Native Buildpacks/bbl/vars' -q --attach att-2702221144863353409-84595 > "$dir"/vars/cloud-config-vars.yml
-lpass show 'Shared-Cloud Native Buildpacks/bbl/vars' -q --attach att-2702221144863353409-456 > "$dir"/vars/concourse-vars-file.yml
-lpass show 'Shared-Cloud Native Buildpacks/bbl/vars' -q --attach att-2702221144863353409-17759 > "$dir"/vars/director-vars-file.yml
-lpass show 'Shared-Cloud Native Buildpacks/bbl/vars' -q --attach att-2702221144863353409-69299 > "$dir"/vars/director-vars-store.yml
-lpass show 'Shared-Cloud Native Buildpacks/bbl/vars' -q --attach att-2702221144863353409-57711 > "$dir"/vars/jumpbox-state.json.old
-lpass show 'Shared-Cloud Native Buildpacks/bbl/vars' -q --attach att-2702221144863353409-26545 > "$dir"/vars/jumpbox-vars-file.yml
-lpass show 'Shared-Cloud Native Buildpacks/bbl/vars' -q --attach att-2702221144863353409-20696 > "$dir"/vars/jumpbox-vars-store.yml
-lpass show 'Shared-Cloud Native Buildpacks/bbl/vars' -q --attach att-2702221144863353409-93249 > "$dir"/vars/terraform.tfstate
-lpass show 'Shared-Cloud Native Buildpacks/bbl/vars' -q --attach att-2702221144863353409-45337 > "$dir"/vars/terraform.tfstate.backup
+lpass show 'Shared-Cloud Native Buildpacks/bbl/vars' -q --attach att-2702221144863353409-57845 > "$dir"/vars/bbl.tfvars
+lpass show 'Shared-Cloud Native Buildpacks/bbl/vars' -q --attach att-2702221144863353409-48962 > "$dir"/vars/bosh-state.json
+lpass show 'Shared-Cloud Native Buildpacks/bbl/vars' -q --attach att-2702221144863353409-85040 > "$dir"/vars/cloud-config-vars.yml
+lpass show 'Shared-Cloud Native Buildpacks/bbl/vars' -q --attach att-2702221144863353409-25035 > "$dir"/vars/concourse-vars-file.yml
+lpass show 'Shared-Cloud Native Buildpacks/bbl/vars' -q --attach att-2702221144863353409-17643 > "$dir"/vars/director-vars-file.yml
+lpass show 'Shared-Cloud Native Buildpacks/bbl/vars' -q --attach att-2702221144863353409-33876 > "$dir"/vars/director-vars-store.yml
+lpass show 'Shared-Cloud Native Buildpacks/bbl/vars' -q --attach att-2702221144863353409-42893 > "$dir"/vars/jumpbox-state.json
+lpass show 'Shared-Cloud Native Buildpacks/bbl/vars' -q --attach att-2702221144863353409-47949 > "$dir"/vars/jumpbox-vars-file.yml
+lpass show 'Shared-Cloud Native Buildpacks/bbl/vars' -q --attach att-2702221144863353409-79905 > "$dir"/vars/jumpbox-vars-store.yml
+lpass show 'Shared-Cloud Native Buildpacks/bbl/vars' -q --attach att-2702221144863353409-82344 > "$dir"/vars/terraform.tfstate

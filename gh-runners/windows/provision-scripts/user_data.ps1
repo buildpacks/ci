89a089ec5c1df6552304e23c9efa0f0d7c2d92da
Javier Romero <rjavier@vmware.com>
2022-01-20 09:23:05 -0600

    change password before restart to prevent race condition
    Signed-off-by: Javier Romero <rjavier@vmware.com>

--- gh-runners/windows/provision-scripts/user_data.ps1
+++ gh-runners/windows/provision-scripts/user_data.ps1
@@ -5,8 +5,8 @@ Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 Set-Service -Name sshd -StartupType Automatic
 Start-Service sshd
 
-# Install Hyper-V for Docker; NOTE: This will restart the machine.
-Install-WindowsFeature -Name Hyper-V -IncludeManagementTools -Restart
-
 # Change Admin Password
 Set-LocalUser -Name "${admin_username}" -Password (ConvertTo-SecureString "${admin_password}" -AsPlainText -Force)
+
+# Install Hyper-V for Docker; NOTE: This will restart the machine.
+Install-WindowsFeature -Name Hyper-V -IncludeManagementTools -Restart
